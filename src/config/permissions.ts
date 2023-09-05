c4b2982410e27ddb8b930d6aea0ac68293ed6017
itsyaboikris <kristoff_riley@outlook.com>
2023-09-04 22:01:29 -0400

    added roles and permissions

---
+++ src/config/permissions.ts
@@ -0,0 +1,24 @@
+export const ALL_PERMISSIONS = [
+
+	'users:roles:write', // Allow to add role to user
+	'users:roles:delete', // allow to remove role from user
+
+	'post:write',
+	'post:read',
+] as const
+
+export const PERMISSIONS = ALL_PERMISSIONS.reduce((acc, permission) => {
+	acc[permission] = permission
+	
+	return acc
+}, {} as Record<(typeof ALL_PERMISSIONS)[number],(typeof ALL_PERMISSIONS)[number] >)
+
+export const USER_ROLE_PERMISSIONS = [
+	PERMISSIONS['post:write'],
+	PERMISSIONS['post:read']
+]
+
+export const SYSTEM_ROLES = {
+	SUPER_ADMIN: 'SUPER_ADMIN',
+	APPLICATION_USER: 'APPLICATION_USER',
+}
--- src/modules/applications/applications.routes.ts
+++ src/modules/applications/applications.routes.ts
@@ -1,9 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { createApplicationHandler } from "./applications.controllers";
 import { createApplicationJsonSchema } from "./application.schemas";
+import { getApplications } from "./applications.services";
 
 export async function applicationRoutes(app:FastifyInstance) {
 	app.post('/',{schema: createApplicationJsonSchema}, createApplicationHandler)
 
-	app.get('/', () => {} )
+	app.get('/', getApplications)
 }
